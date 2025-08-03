46d8d9196cea2673ca6eacdb2e3ca9cc06c07374
patgar-suraj <surajrp7012@gmail.com>
2025-08-03 20:12:36 +0530

    initialize base structure

---
+++ script.js
@@ -0,0 +1,15 @@
+let elems = document.querySelectorAll(".elem")
+let fullElem = document.querySelectorAll(".fullElem")
+let fullElemBackBtn = document.querySelectorAll(".fullElem .backBtn")
+
+elems.forEach(function(elem) {
+    elem.addEventListener("click", function() {
+        fullElem[elem.id].style.display = "block"
+    })
+})
+
+fullElemBackBtn.forEach(function(backBtn, id) {
+    backBtn.addEventListener("click", function() {
+        fullElem[id].style.display = "none"
+    })
+})
