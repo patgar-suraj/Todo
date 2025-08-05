bab80a72d99e45994a6ff2903222597396c784cc
patgar-suraj <surajrp7012@gmail.com>
2025-08-05 19:29:11 +0530

    create todo list

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let elems = document.querySelectorAll(".elem")
-let fullElem = document.querySelectorAll(".fullElem")
-let fullElemBackBtn = document.querySelectorAll(".fullElem .backBtn")
+function openPages() {
+  let elems = document.querySelectorAll(".elem");
+  let fullElem = document.querySelectorAll(".fullElem");
+  let fullElemBackBtn = document.querySelectorAll(".fullElem .backBtn");
 
-elems.forEach(function(elem) {
-    elem.addEventListener("click", function() {
-        fullElem[elem.id].style.display = "block"
-    })
-})
+  elems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      fullElem[elem.id].style.display = "block";
+    });
+  });
 
-fullElemBackBtn.forEach(function(backBtn, id) {
-    backBtn.addEventListener("click", function() {
-        fullElem[id].style.display = "none"
-    })
-})
+  fullElemBackBtn.forEach(function (backBtn, id) {
+    backBtn.addEventListener("click", function () {
+      fullElem[id].style.display = "none";
+    });
+  });
+}
+openPages()
+// ////////////////////////////////////////////////////////////
+
